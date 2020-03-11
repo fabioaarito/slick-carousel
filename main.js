f7a9c5190e9ca71765849ad16ce8a00bd2bf0331
Fábio Rito <fabioaarito@gmail.com>
2020-03-11 08:46:48 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".carousel").slick({
+    dots: true,
+    infinite: true,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    easing: true,
+    edgeFriction: true,
+    cssEase: 'linear'
+  });
+});
